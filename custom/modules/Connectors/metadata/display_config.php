b139662e99b899627479e8f5b90794c2dac93006
Shoaib <s.fazal16@gmail.com>
2015-10-07 00:26:51 +0500

    Initial Commit Clean SUGARCRM

---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2015-10-06 21:16:50
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
